67cb0eb212461f2a82240b9adaeb79e8f2170e5a
Rohun Saxena <rohuns@stanford.edu>
2017-12-04 13:51:45 -0800

    checking pipeline

--- csp.py
+++ csp.py
@@ -258,6 +258,7 @@ class BacktrackingSearch():
                         g = genres_map[assignment['genre']]
 
                     rating = forest.predict([[randint(30000000, 40000000),content_r,d_name,a3_name,a2_name,a1_name,g]])[0]
+                    print assignment
                     self.backtrack(assignment, numAssigned + 1, rating * weight * deltaWeight)
                     # restore the previous domains
                     self.domains = localCopy
--- csp_eval.py
+++ csp_eval.py
@@ -27,18 +27,21 @@ def makeProfile(m):
 actors_map = pickle.load(open("actors.pickle", 'rb'))
 directors_map = pickle.load(open("directors.pickle", "rb"))
 actorBulletin, directorBulletin = util.ActorBulletin(actors_map), util.DirectorBulletin(directors_map) #load pickles
-assignments = pickle.load(open("fileMapping.pickle", "rb")) #partial assignments to test
-#assignments = ["sample_profile.txt", "sample_profile_2.txt"]
+#assignments = pickle.load(open("fileMapping.pickle", "rb")) #partial assignments to test
+assignments = ["sample_profile.txt", "sample_profile_2.txt"]
 
 completed = []
-print assignments.keys()[0]
-for p in [assignments.keys()[0]]:
+for p in assignments:
 	profile = util.Profile(actorBulletin, directorBulletin, "profiles/" + p)
 	cspConstructor = csp.MovieCSPConstructor(actorBulletin, directorBulletin, profile)
 	csp_1 = cspConstructor.get_basic_csp()
 	alg = csp.BacktrackingSearch()
-	alg.solve(csp_1, mcv=True, ac3=True, budget=profile.budget)
+	if profile.budget == None:
+		alg.solve(csp_1, mcv=True, ac3=True)
+	else:
+		alg.solve(csp_1, mcv=True, ac3=True, budget=profile.budget)
 	completedProfile = makeProfile(alg.optimalAssignment)
 	truth = util.Profile(actorBulletin, directorBulletin, "profiles/" + assignments[part])
 	completed.append(completedProfile, truth)
+	print alg.optimalAssignment
 print(assignments_error.error(done))
