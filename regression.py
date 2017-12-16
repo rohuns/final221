2c625243af9d22232c3a1ab474247363b564453a
Veeral Patel <veeral@paribus.co>
2017-12-15 22:51:34 -0500

    Print coefficients

--- regression.py
+++ regression.py
@@ -17,10 +17,11 @@ movies = pd.read_csv('final_data.csv')
 X = movies[['budget', 'content_rating', 'director_name', 'actor_3_name', 'actor_2_name', 'actor_1_name', 'genres']]
 y = movies['imdb_score']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 regression_model = linear_model.LinearRegression()
 regression_model.fit(X_train, y_train)
+print regression_model.coef_
 
 y_predict_lr = regression_model.predict(X_test)
 print 'Linear Regression Mean squared error: {}'.format(math.sqrt(mean_squared_error(y_predict_lr, y_test)))
@@ -39,6 +40,7 @@ plt.show()
 
 lasso = linear_model.Lasso()
 lasso.fit(X_train, y_train)
+print lasso.coef_
 
 y_predict_lasso = lasso.predict(X_test)
 print 'Lasso Mean squared error: {}'.format(math.sqrt(mean_squared_error(y_predict_lasso, y_test)))
@@ -57,6 +59,7 @@ plt.show()
 
 ridge = linear_model.Ridge()
 ridge.fit(X_train, y_train)
+print ridge.coef_
 
 y_predict_ridge = ridge.predict(X_test)
 print 'Ridge Mean squared error: {}'.format(math.sqrt(mean_squared_error(y_predict_ridge, y_test)))
@@ -75,10 +78,12 @@ plt.show()
 
 forest = RandomForestRegressor()
 forest.fit(X_train, y_train)
+print forest.feature_importances_
 
 y_predict_forest = forest.predict(X_test)
 print 'Forest Mean squared error: {}'.format(math.sqrt(mean_squared_error(y_predict_forest, y_test)))
 print 'Forest Median absolute error: {}'.format(median_absolute_error(y_predict_forest, y_test))
+
 
 joblib.dump(forest, 'model.pkl') 
 
